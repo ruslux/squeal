22fd162978d6a0a8829d7272e3646a2cf6816c1b
Roman Ilin <romanilin@email.com>
2018-04-01 01:26:54 +0300

    Add /src to python start path

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ cd $(dirname "$0")
 BASEDIR=$(pwd)
 mkdir -p data/db
 mongod --dbpath "${BASEDIR}/data/db" > /dev/null 2>mongodb.log &
-python3 "${BASEDIR}" > /dev/null 2>python.log &
+python3 "${BASEDIR}/src" > /dev/null 2>python.log &
 sh "${BASEDIR}/issue_tracker.sh" > /dev/null 2>&1 &
